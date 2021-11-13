8d59c517459400f9d92508cff95f3f398605f318
Nat Koyama <59725521+xpinocchio@users.noreply.github.com>
2021-11-14 00:25:40 +0900

    Add email_validator to forms.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, g
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
