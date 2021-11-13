64d27aed5474d2766a53224f3a40b8fc03eafd81
Nat Koyama <59725521+xpinocchio@users.noreply.github.com>
2021-11-14 00:31:29 +0900

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
-import email_validator
 
 
 ##WTForm
